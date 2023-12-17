3194f7bd18f673c26d2b68c31d96e364da9e3e24
yangheat <yangheat@gamil.com>
2023-12-17 19:09:04 +0900

    메인 화면 전용 스타일 적용되도록 수정

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import './index.css'
 
 export default function Home() {
   return (
@@ -40,15 +41,6 @@ export default function Home() {
         />
       </div>
       
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        {/* <img src='/images/profile.jpg' alt='profile' /> */}
-        {/* <Image
-          src='/images/profile.jpg'
-          alt='profile'
-          width={140}
-          height={140}
-        /> */}
-      </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <Link href="/post/first-post" className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
@@ -145,27 +137,7 @@ export default function Home() {
           </p>
         </a>
         
-        <Link
-          href="/post/second-post"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Second Post (Link Component)
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            The first page I created with next.js
-          </p>
-        </Link>
         
-        <Image
-          src='/images/profile.jpg'
-          alt='profile'
-          width={140}
-          height={140}
-        />
       </div>
     </main>
   )
