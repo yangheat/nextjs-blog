eb77e316f6ab788a45527fe30db4860c4cf6bdda
yangheat <yangheat@gamil.com>
2024-01-21 15:15:46 +0900

    mdx, syntax-highlight 를 이용하여 마크다운에서  코드블록 UI 적용

--- src/app/post/[id]/page.js
+++ src/app/post/[id]/page.js
@@ -1,25 +1,28 @@
-import Date from "../../components/date"
-import { getAllPostIds, getPostData } from "../../../../lib/posts"
-import utilStyles from '../styles/utils.module.css';
+import Date from '../../components/date'
+import MdxContent from '../../components/MdxContent'
+import { getAllPostIds, getPostData } from '../../../../lib/posts'
+import utilStyles from '../styles/utils.module.css'
 
-export function generateStaticParams () {
-    const params = getAllPostIds()
-    return params.map((param) => ({
-        id: param.id
-    }))
+export function generateStaticParams() {
+  const params = getAllPostIds()
+  return params.map((param) => ({
+    id: param.id,
+  }))
 }
 
 export default async function Page({ params }) {
-    const { id } = params
-    
-    const postData = await getPostData(id)
+  const { id } = params
+  const postData = await getPostData(id)
   return (
     <>
       <h1 className={utilStyles.headingXl}>{postData.title}</h1>
       <div className={utilStyles.lightText}>
         <Date dateString={postData.date} />
       </div>
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      {postData.mdExtContentHtml && (
+        <div dangerouslySetInnerHTML={{ __html: postData.mdExtContentHtml }} />
+      )}
+      {postData.mdxSource && <MdxContent source={postData.mdxSource} />}
     </>
   )
 }
