0702c9f89e3ec3407cb6c1fd2e406315310fb2ea
yangheat <yangheat@gamil.com>
2024-02-18 18:01:32 +0900

    게시물 화면에 에러 페이지 추가

---
+++ src/app/post/[id]/error.js
@@ -0,0 +1,14 @@
+'use client'
+
+import { useEffect } from 'react'
+
+export default function Error(error, reset) {
+  useEffect(() => {
+    console.error(error)
+  }, [error])
+  return (
+    <>
+      <h1>페이지를 찾을 수 없습니다.(URL을 확인해주세요.)</h1>
+    </>
+  )
+}
