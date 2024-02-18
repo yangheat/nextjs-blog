fb69384ef54eb720b06d56cd24f365308ce0a7a6
yangheat <yangheat@gamil.com>
2024-02-18 21:09:25 +0900

    reset 파라미터 사용하여 새로고침 버튼 추가

--- src/app/post/[id]/error.js
+++ src/app/post/[id]/error.js
@@ -1,5 +1,6 @@
 'use client'
 
+import Button from '@/components/Button'
 import { useEffect } from 'react'
 
 export default function Error(error, reset) {
@@ -9,6 +10,7 @@ export default function Error(error, reset) {
   return (
     <>
       <h1>페이지를 찾을 수 없습니다.(URL을 확인해주세요.)</h1>
+      <Button onClick={() => reset()}>Try again</Button>
     </>
   )
 }
